a611cf6149127fe08d084ce5f53bde3d0e7d874c
thanhvinh199700 <thanhvinh199700@gmail.com>
2019-09-20 12:51:57 +0700

    update

--- resources/views/layouts/product.blade.php
+++ resources/views/layouts/product.blade.php
@@ -274,7 +274,7 @@ $(document).ready(function () {
                     @foreach($menus as $menu)
                     <a href="{{$menu->menu_link}}"><b style="font-family: Tahoma">{{$menu->menu_name}}</b></a>
                     @endforeach
-                    <?php dd($categories);?>
+                    
                     @foreach($categories as $category)
                     <a href="/cat/{{$category->id}}"><b style="font-family: Tahoma">{{$category->category_name}}</b></a>
                     @endforeach
