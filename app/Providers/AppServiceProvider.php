f3b829f6fe5ce8a2e68c7fb080bf115e3e849edb
thanhvinh199700 <thanhvinh199700@gmail.com>
2019-09-20 15:45:59 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function boot() {
          //
-        view()->composer(['layouts.app','layouts.product','home.order','account.mail'],function($view)
+        view()->composer(['layouts.app','layouts.product','frontend.order','account.mail'],function($view)
         {
             if(Session::has('cart'))
             {
