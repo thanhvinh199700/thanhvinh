d37cc80b94cd78be25895e50ab5d1e3b24f31aed
thanhvinh199700 <thanhvinh199700@gmail.com>
2019-09-23 12:08:45 +0700

    test

--- resources/views/account/send_promotion_mail.blade.php
+++ resources/views/account/send_promotion_mail.blade.php
@@ -1,4 +1,3 @@
 <h1 style="color: red;">THÔNG BÁO KHUYẾN MÃI ĐẾN TOÀN THỂ KHÁCH HÀNG </h1>
-<h3>{{$short}} truy cập vào <a href="http://homestead.test/" style="text-decoration: none;">http://homestead.test/</a> để xem chi tiết</h3>
-<p>{!!$content!!}</p>
+<h3><a href="http://homestead.test/" style="text-decoration: none;">http://homestead.test/</a> để xem chi tiết</h3>
 
