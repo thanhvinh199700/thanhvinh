2a5c1676a6f4597b15e9fd50ab4670f2078d007d
thanhvinh199700 <thanhvinh199700@gmail.com>
2019-09-23 10:22:41 +0700

    test

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model {
 
-    protected $table = 'order';
+    protected $table = 'orders';
     protected $primaryKey = 'id';
     protected $fillable = ['order_date', 'full_name', 'email', 'address', 'phone', 'trash', 'status', 'total_price', 'total_quantity'];
 
