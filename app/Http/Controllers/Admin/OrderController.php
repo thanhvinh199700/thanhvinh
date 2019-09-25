c2f93fdd17988928a991fe164b36b14b1f9215c2
thanhvinh199700 <thanhvinh199700@gmail.com>
2019-09-25 10:33:32 +0700

    fix loi

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -8,46 +8,53 @@ use App\Models\Order;
 use App\Exports\OrdersExport;
 use App\Exports\OrderDetailExport;
 use Maatwebsite\Excel\Facades\Excel;
-class OrderController extends Controller {
+
+class OrderController extends Controller
+{
 
     protected $orderService;
 
-    public function __construct(OrderService $orderService) {
+    public function __construct(OrderService $orderService)
+    {
         $this->orderService = $orderService;
     }
 
-    public function index() {
+    public function index()
+    {
 
         $orders = $this->orderService->getOrder();
         return view('admin.order.index', ['order' => $orders]);
     }
 
-    public function changeTrash(Order $order_id) {
+    public function changeTrash(Order $order_id)
+    {
         $this->orderService->trash($order_id);
         return redirect('orders');
     }
 
-    public function destroy(Order $Order) {
-        try {
+    public function destroy(Order $Order)
+    {
 
-            $this->orderService->deleteOrder($Order);
-            return redirect('orders')->with('success_message', 'delete order sucess');
-        } catch (\Exception $e) {
-            return redirect('orders')->with('error_message', $e->getMessage());
-        }
+
+        $this->orderService->deleteOrder($Order);
+        return redirect('orders')->with('error_message', 'đã xóa thành công');
+
     }
 
-    public function detail(Order $order) {
+    public function detail(Order $order)
+    {
         $details = $this->orderService->detail($order);
         $info = $this->orderService->infoDetail($order);
         return view('admin.order.detail', ['info' => $info, 'detail' => $details]);
     }
 
-    public function export() {
+    public function export()
+    {
         return Excel::download(new OrdersExport, 'orders.xlsx');
     }
 
-    public function detailExport(Order $order) {
+    public function detailExport(Order $order)
+    {
         return Excel::download(new OrderDetailExport($order), 'orders_detail.xlsx');
     }
 
